3dcf7d6b34cd6077aa6d421f7f69ea2a91321ddd
lucianob <109104409+lugengar@users.noreply.github.com>
2026-01-23 00:37:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ function updatePrevisualizacion(){
   if(tipoRegaloVirtual && mensaje2){
     previsualizacionContainer.style.display = "block";
     previsualizacionContainer.innerHTML = `
-      <a href="https://lubobu.com.ar/sorpresa/${tipoRegaloVirtual}/${encodeURIComponent(mensaje2)}" class="boton" target="_blank">
+      <a href="https://lubobu.com.ar/sorpresa/${tipoRegaloVirtual}/${encodeURIComponent(mensaje2)}" target="_blank">
        Previsualizar regalo
       </a>
     `;
